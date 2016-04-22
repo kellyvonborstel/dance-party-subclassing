399820fb2f54360e2ea76b197ad33f59f5734880
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-21 17:47:56 -0700

    modify init.js to position dancers randomly

--- src/init.js
+++ src/init.js
@@ -11,7 +11,18 @@ $(document).ready(function() {
     // get the maker function for the kind of dancer we are making
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    var heigth = 200;
+    var height;
+    
+    // use modulo so dancers will be positioned at different heights
+    if (window.dancers.length % 3 === 0) {
+      height = $('.dance-area').height() - 460;
+    } else if (window.dancers.length % 2 === 0) {
+      height = $('.dance-area').height() - 420;
+    } else {
+      height = $('.dance-area').height() - 380;
+    }
+
+    // make a dancer with a random x-position and staggered y-position
     var dancer = new dancerMakerFunction(height, $('.dance-area').width() * Math.random() - 120, 1000);
 
     window.dancers.push(dancer);
